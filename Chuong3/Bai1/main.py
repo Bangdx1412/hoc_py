aebd25be009dc54e509d628986751fd8dd7dfb70
Bangdx1412 <doxuanbang14122005@gmail.com>
2026-03-24 15:15:27 +0700

    Xong chương 3

---
+++ Chuong3/Bai1/main.py
@@ -0,0 +1,6 @@
+import math as mt #import ten_thu_vien as bi_danh
+import numpy as np
+a = mt.sqrt(16) # ten_thu_vien.ten_ham(đối số của hàm)
+b = mt.pow(12,2)
+c = np.array([1,2,3,4,5])
+print(c)
---
+++ Chuong3/Bai2/baitap.py
@@ -0,0 +1,5 @@
+# Tinh thể tích hình cầu V = (4/3) * π * r³
+import math as mt
+r = float(input("Nhập bán kính hình cầu: "))
+v = (4/3) * mt.pi * r**3
+print(round(v,2))
