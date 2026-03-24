5a94beefe4a514ac3d1b7da642275f26a5e3aff4
Bangdx1412 <doxuanbang14122005@gmail.com>
2026-03-24 14:34:26 +0700

    Xong chương 2

---
+++ Chuong2/KieuDuLieuSo/b2_cac_phep_toan_thao_tac_ten_bit.py
@@ -0,0 +1,17 @@
+# Các phép toán thao tác bit cơ bản
+# Phép toán AND (&)
+# Phép toán OR (|)
+# Phép toán XOR (^)
+# Phép toán NOT (~)
+# Toán tử dịch bit 
+# Dịch phải (>>) a/2 ^ n
+# Dịch trái bit (<<) = a * 2^n vd a = 10 print(a<<3) => 10 * 2 ^ 3
+
+a = 10
+b = 2
+print(a & b)
+print(a | b)
+print(a ^ b)
+print(~b) # -(n+1)
+print(a<<3)
+print(a>>3)
---
+++ Chuong2/KieuDuLieuSo/baitap.py
@@ -0,0 +1,40 @@
+import math
+
+# x = 0.501
+# f = pow(math.e,x) - x
+# print(math.e**x - x)
+# round: Làm tròn số
+# print(round(f,2))
+
+# Nhập số y
+# y = int(input("Enter x: "))
+# kq1 = y ** 2
+# kq2 = y ** 3
+# kq3 = y ** 4
+
+# print(kq1,kq2,kq3)
+
+# 2.3
+# f = float(input("Nhap f: "))
+# c = (f-32)/1.8
+# print(round(c,2))
+
+# s = input("Nhập đoạn string của bạn: ")
+# res = ""
+
+# if len(s) < 2 :
+#     res = " "
+# else :
+#     s1 = s[0:2]
+#     s2 = s[-2:]
+#     res = s1+s2
+    
+# print(res)
+
+# Bài 2.4
+# Đổi chỗ
+a =10 
+b = 20
+
+a,b = b,a
+print(a,b)
