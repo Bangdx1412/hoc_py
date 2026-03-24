b047fa9070b86e08ac8f00b6db4f3933eace69ce
Bangdx1412 <doxuanbang14122005@gmail.com>
2026-03-24 10:18:44 +0700

    Toán tử cơ bản

---
+++ Chuong2/KieuDuLieuSo/b1.py
@@ -0,0 +1,23 @@
+a = 10 #số nguyên
+b= 10.4 # số thực
+c = 5e2 #5 x 10^2
+d = 10//3 # Chia lấy nguyên
+e = 10**3 # Lũy thừa
+print(c)
+
+# 
+# so sánh bằng ==
+#lớn hơn >
+# nhỏ hơn <
+# lớn hoặc bằng >=
+# Nhỏ hơn hoặc bằng <=
+# So sánh khác !=
+
+# Toán logic
+# and, or, not
+
+l = 10
+l+=1
+
+m = 20
+m**=2 # m = m ** 2
