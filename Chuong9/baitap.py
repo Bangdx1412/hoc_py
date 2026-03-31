5ba36d5ed9af3ad0bf44561695bfd1ab1acf0e3c
Bangdx1412 <doxuanbang14122005@gmail.com>
2026-03-31 10:38:14 +0700

    Bài tập

--- Chuong9/baitap.py
+++ Chuong9/baitap.py
@@ -1,3 +1,13 @@
-with open("Chuong9/SinhVien.txt") as file :
-    s = file.read()
+with open("Chuong9/SinhVien.txt","r", encoding="utf-8") as file :
+    r = file.read()
+    print(r)
+# with open("Chuong9/SinhVien.txt","w",encoding="utf-8") as file2:
+#     ma = input("MÃ: ")
+#     ten = input("Họ và tên: ")
+#     file2.writelines([ma,ten])
+    
+# Đọc danh sách
+with open("Chuong9/SinhVien.txt", "r", encoding="utf-8") as file3:
+    s = file3.read() # Thêm dấu () ở đây
+    print("\nDanh sách sau khi cập nhật:")
     print(s)
--- Chuong9/tempCodeRunnerFile.py
+++ Chuong9/tempCodeRunnerFile.py
@@ -1,1 +1,8 @@
-SinhVien.txt
+with open("Chuong9/SinhVien.txt") as file :
+    s = file.read()
+    print(s)
+with open("Chuong9/SinhVien.txt","w",encoding="utf-8") as file2:
+    ma = input("MÃ: ")
+    ten = input("Họ và tên: ")
+    file2.writelines([ma,ten])
+    
